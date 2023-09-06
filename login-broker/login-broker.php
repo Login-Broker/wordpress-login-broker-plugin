18e6decdb199e07dbcd72c61383df327026cabd9
gyxi <nb@gyxi.com>
2023-09-06 09:03:28 +0200

    Adjusted plugin definition

--- login-broker/login-broker.php
+++ login-broker/login-broker.php
@@ -1,9 +1,14 @@
 <?php
 
 /*
-Plugin Name: Login Broker
-Description: Allow people to sign up and become 'Subscriber' in your Wordpress site via social logins with Google, Facebook, Apple, Microsoft, Linkedin and more.
-Author: Gyxi
-*/
+ * Plugin Name:       Login Broker
+ * Plugin URI:        https://login.broker
+ * Description:       Allow people to sign up and become 'Subscriber' in your Wordpress site via social logins with Google, Facebook, Apple, Microsoft, Linkedin and more.
+ * Version:           0.9
+ * Author:            Gyxi
+ * Author URI:        https://gyxi.com
+ * License:           GPL v2
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
 require_once plugin_dir_path(__FILE__) . 'includes/wpp-functions.php';
